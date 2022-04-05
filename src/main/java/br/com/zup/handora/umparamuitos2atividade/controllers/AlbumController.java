015eb2abb2cc480cac369f9b50cb20c70f1d4e33
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-05 16:09:46 -0300

    Add album creation method to controller
    and delete the Figurinha repository.

--- src/main/java/br/com/zup/handora/umparamuitos2atividade/controllers/AlbumController.java
+++ src/main/java/br/com/zup/handora/umparamuitos2atividade/controllers/AlbumController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.umparamuitos2atividade.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.umparamuitos2atividade.models.Album;
+import br.com.zup.handora.umparamuitos2atividade.models.AlbumDTO;
 import br.com.zup.handora.umparamuitos2atividade.repositories.AlbumRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class AlbumController {
 
     public AlbumController(AlbumRepository albumRepository) {
         this.albumRepository = albumRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid AlbumDTO albumDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Album album = albumRepository.save(albumDTO.toModel());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(album.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
--- src/main/java/br/com/zup/handora/umparamuitos2atividade/repositories/FigurinhaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.zup.handora.umparamuitos2atividade.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.zup.handora.umparamuitos2atividade.models.Figurinha;
-
-public interface FigurinhaRepository extends JpaRepository<Figurinha, Long> {
-
-}
