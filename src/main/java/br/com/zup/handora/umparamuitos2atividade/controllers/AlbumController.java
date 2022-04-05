fcad63e2559f5856cae863d0d275f34bd264a412
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-05 16:07:25 -0300

    Add repository dependency to Album controller

--- src/main/java/br/com/zup/handora/umparamuitos2atividade/controllers/AlbumController.java
+++ src/main/java/br/com/zup/handora/umparamuitos2atividade/controllers/AlbumController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.umparamuitos2atividade.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.umparamuitos2atividade.repositories.AlbumRepository;
+
 @RestController
 @RequestMapping(AlbumController.BASE_URI)
 public class AlbumController {
 
     public final static String BASE_URI = "/albuns";
+
+    private final AlbumRepository albumRepository;
+
+    public AlbumController(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
 
 }
