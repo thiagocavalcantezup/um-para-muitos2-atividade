9e9f1d510d60d217f138b48f53210ac2d4f445ff
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-05 15:58:52 -0300

    Add repositories

---
+++ src/main/java/br/com/zup/handora/umparamuitos2atividade/repositories/FigurinhaRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.umparamuitos2atividade.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.umparamuitos2atividade.models.Figurinha;
+
+public interface FigurinhaRepository extends JpaRepository<Figurinha, Long> {
+
+}
