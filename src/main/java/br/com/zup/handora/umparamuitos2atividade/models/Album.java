258130ef9784231c1208d8e3b74cf7fbb57ecc70
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-05 15:40:19 -0300

    Add Album and Figurinha entity classes

---
+++ src/main/java/br/com/zup/handora/umparamuitos2atividade/models/Album.java
@@ -0,0 +1,59 @@
+package br.com.zup.handora.umparamuitos2atividade.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Entity
+@Table(name = "albuns")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false, length = 500)
+    private String descricao;
+
+    @Column(nullable = false)
+    @Min(10)
+    @Max(100)
+    private Integer numeroDePaginas;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "album")
+    private Set<Figurinha> figurinhas = new HashSet<>();
+
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Album() {}
+
+    public Album(String titulo, String descricao, @Min(10) @Max(100) Integer numeroDePaginas) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Set<Figurinha> getFigurinhas() {
+        return figurinhas;
+    }
+
+}
---
+++ src/main/java/br/com/zup/handora/umparamuitos2atividade/models/Figurinha.java
@@ -0,0 +1,47 @@
+package br.com.zup.handora.umparamuitos2atividade.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "figurinhas")
+public class Figurinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer pagina;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @ManyToOne(optional = false)
+    private Album album;
+
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Figurinha() {}
+
+    public Figurinha(Integer pagina, String descricao) {
+        this.pagina = pagina;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+}
